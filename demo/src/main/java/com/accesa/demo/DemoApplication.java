05b5cd2261a0af0d04d7690a23f0c35b53ea7b52
darius <dariusboengiu@gmail.com>
2023-04-05 09:26:40 +0300

    Initial commit

---
+++ demo/src/main/java/com/accesa/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.accesa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/accesa/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accesa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
